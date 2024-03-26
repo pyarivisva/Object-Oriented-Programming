91a11aec66177498d36db7c8e701adf6738d5c3b
Pyarivisvapujitadevidasi <pyarivisvapujita1@gmail.com>
2024-03-26 16:11:13 +0800

    first commit

---
+++ LoginAndSignUp/src/loginandsignup/LoginAndSignup.java
@@ -0,0 +1,14 @@
+package loginandsignup;
+
+
+public class LoginAndSignup {
+
+    
+    public static void main(String[] args) {
+       Login LoginFrame = new Login();
+       LoginFrame.setVisible(true);
+       LoginFrame.pack();
+       LoginFrame.setLocationRelativeTo(null);
+    }
+    
+}
